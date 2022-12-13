ba1949ab6df1bbebde7ba692df0c5ec7b3672862
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2022-12-13 11:33:42 -0500

    Add files via upload

--- circle.h
+++ circle.h
@@ -2,13 +2,15 @@
 #include <cstdio>
 #include <cmath>
 
-class circle{
-  public :
-    void populate_classObj(double, double, double, double);
-    double getRadius(double, double, double, double);
-    double getCircumference(double, double, double, double);
-    double getArea(double, double, double, double);
-  protected :
-    double getDistance(double, double, double, double);
-  private :
-    double x1, y1, x2, y2;
+class circle
+{
+	public:
+		void populate_classObj(double, double, double, double);
+		double getRadius(double, double, double, double);
+		double getCircumference(double, double, double, double);
+		double getArea(double, double, double, double);
+	protected:
+		double getDistance(double, double, double, double);
+	private:
+		double x1, y1, x2, y2;
+};
--- main.cc
+++ main.cc
@@ -3,44 +3,49 @@
 #include <cmath>
 #include <circle.h>
 #define PI 3.1415926535
-
 using namespace std;
 
-int main(){
-  circle circ1;
-  double x1, y1, x2, y2;
-  int value;
-  cout << "Please enter your first coordinates (center of circle) : \n";
-  cin >> x1;
-  cin >> y1;
-  circ1.populate_classObj(x1,y1,x2,y2);
-  
+
+int main()
+{
+	circle circ1;	//creates new object
+	double x1, y1, x2, y2;
+	int switchinput;
+	cout << "Please enter your first coordinates (center of circle) line by line : " << endl;
+	cin >> x1;
+	cin >> y1;
+	cout << "Please enter your second coordinates line by line : " << endl;
+	cin >> x2;
+	cin >> y2;
+	circ1.populate_classObj(x1, y1, x2, y2);
+
 do{
-  cout << "\n Pick a function : \n";
-  cout << "1. Calculate the distance \n";
-  cout << "2. Calculate the circumference \n";
-  cout << "3. Calculate the area \n";
-  cout << "4. Exit the program \n";
-  cin >> value; //switch value
-  
-  switch(value){
-    case 1 : //radius
-      cout << circ1.getRadius(x1,y1,x2,y2) << "\n";
-      break;
-    case 2 : // circumference
-      cout << circ1.getCircumference(x1,y1,x2,y2) << "\n";
-      break;
-    case 3 : //area
-      cout << circ1.getArea(x1,y1,x2,y2) << "\n";
-      break;
-    case 4 : //exit
-      cout << "Thank you for using the program!" << "\n";
-      return 0;
-      break;
-    default : 
-      cout << "Invalid input. \n";
-      break;
-  }
+	cout << endl << "Pick a function : " << endl;
+	cout << "1. Calculate the distance" << endl;
+	cout << "2. Calculate the circumference" << endl;
+	cout << "3. Calculate the area" << endl;
+	cout << "4. Exit the program" << endl;
+	cin >> switchinput; //switch option
+
+	switch(switchinput){
+		case 1 : //radius
+			cout << "Distance = " << circ1.getRadius(x1, y1, x2, y2) << endl;
+			break;
+		case 2 :  //circumference
+			cout << "Circumference = " << circ1.getCircumference(x1, y1, x2, y2) << endl;
+			break;
+		case 3 : //area
+			cout << "Area = " << circ1.getArea(x1, y2, x2, y2) << endl;
+			break;
+		case 4 : //exit
+			cout <<"Thank you for using the program!" << endl;
+			return 0;
+			break;
+		default :
+			cout << "Invalid input. \n";
+			break;
+		
+	}
 }
-while(value!=4);
+while(switchinput!=4);
 }
